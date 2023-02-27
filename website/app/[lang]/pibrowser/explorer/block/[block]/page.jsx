22018741416b3d972ca92a65b7fa3e464a0270d2
lazytime <0205miss@gmail.com>
2023-02-27 21:22:24 +0800

    scroll layout fix

--- website/app/[lang]/pibrowser/explorer/block/[block]/page.jsx
+++ website/app/[lang]/pibrowser/explorer/block/[block]/page.jsx
@@ -7,22 +7,24 @@ export default async function BlockPage({params:{lang,block}}){
     const transcript = await import(`locales/${lang}.json`);
     return(
         <>
-        <section className="my-2">
-            <div className="text-center text-2xl font-mono font-semibold border-b mx-4">
-                Block {block}
-                <Share block={block}/>
-            </div>
-            
-            <div className="text-center pb-2 text-purple-500 text-lg my-2 border-b mx-4" >
-                <Dashboard block={block}/>                 
-            </div>
-        </section>
+        <section className=" m-4 overflow-y-scroll h-full pb-28">
+            <section className="my-2">
+                <div className="text-center text-2xl font-mono font-semibold border-b mx-4">
+                    Block {block}
+                    <Share block={block}/>
+                </div>
+                
+                <div className="text-center pb-2 text-purple-500 text-lg my-2 border-b mx-4" >
+                    <Dashboard block={block}/>                 
+                </div>
+            </section>
 
-        <section>
-            <div className="text-center mb-2 font-bold text-lg bg-border bg-border-size bg-no-repeat bg-left-bottom">
-                Transaction
-            </div>
-            <Transaction block={block} transcript={transcript.explorer.transaction}/>
+            <section>
+                <div className="text-center mb-2 font-bold text-lg bg-border bg-border-size bg-no-repeat bg-left-bottom">
+                    Transaction
+                </div>
+                <Transaction block={block} transcript={transcript.explorer.transaction}/>
+            </section>
         </section>
         </>
     )
--- website/app/[lang]/pibrowser/explorer/tx/[tx_hash]/page.jsx
+++ website/app/[lang]/pibrowser/explorer/tx/[tx_hash]/page.jsx
@@ -8,25 +8,27 @@ export default async function TxPage({params:{lang,tx_hash}}){
     const transcript = await import(`locales/${lang}.json`);
     return(
         <>
-        <section className="my-2">
-            <div className="text-center text-2xl font-mono font-semibold border-b mx-4">
-                Transaction 
-                <div className=" break-words">
-                    {tx_hash}
+        <section className=" m-4 overflow-y-scroll h-full pb-28">
+            <section className="my-2">
+                <div className="text-center text-2xl font-mono font-semibold border-b mx-4">
+                    Transaction 
+                    <div className=" break-words">
+                        {tx_hash}
+                    </div>
+                    <Share tx_hash={tx_hash}/>
                 </div>
-                <Share tx_hash={tx_hash}/>
-            </div>
-            
-            <div className="text-center pb-2 text-purple-500 text-lg my-2 border-b mx-4" >
-                <Dashboard tx_hash={tx_hash}/>                 
-            </div>
-        </section>
+                
+                <div className="text-center pb-2 text-purple-500 text-lg my-2 border-b mx-4" >
+                    <Dashboard tx_hash={tx_hash}/>                 
+                </div>
+            </section>
 
-        <section className="mx-2">
-            <div className="text-center mb-2 font-bold text-lg bg-border bg-border-size bg-no-repeat bg-left-bottom">
-                Operation
-            </div>
-           <Operation tx_hash={tx_hash} transcript={transcript.explorer.operation}/>
+            <section className="mx-2">
+                <div className="text-center mb-2 font-bold text-lg bg-border bg-border-size bg-no-repeat bg-left-bottom">
+                    Operation
+                </div>
+            <Operation tx_hash={tx_hash} transcript={transcript.explorer.operation}/>
+            </section>
         </section>
         </>
     )
