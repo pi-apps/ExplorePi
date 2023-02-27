e395bdc1965e847a81b48af7a790abf00c8d9939
lazytime <0205miss@gmail.com>
2023-02-27 21:14:06 +0800

    transaction page

---
+++ website/app/[lang]/pibrowser/explorer/tx/[tx_hash]/op.jsx
@@ -0,0 +1,121 @@
+'use client'
+import { useEffect, useState } from "react"
+import { formatTrans } from "lib/translate"
+import { Server } from "stellar-sdk"
+import Link from "next/link"
+export default function Operation({tx_hash,transcript}){
+    const [lang,setlang] = useState()
+    const server = new Server(process.env['NEXT_PUBLIC_HORIZON_SERVER'])
+    const [data,setdata] = useState(null)
+    useEffect(()=>{
+        setlang(document.documentElement.lang)
+        server.operations()
+        .forTransaction(tx_hash)
+        .call().then( res => {
+            setdata(res.records)
+        })
+    },[tx_hash])
+
+    if(data===null)
+    return(
+        <>
+        <div className="text-center">
+        <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span>
+        </div></div>
+        </>
+    )
+    if(!data.length)
+    return(
+        <>
+        <div className="text-center">
+            There has no Operation
+        </div>
+        </>
+    )
+
+    return(
+        <>
+            <table className="w-full text-center table-fixed font-mono">
+                <thead className="border-b border-slate-400 text-lg">
+                    <tr>
+                        <th>Account</th>
+                        <th>Type</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((data,index)=>{
+                            return(categoryoperation(data,transcript,index,lang))
+                        })
+                    }
+                    
+                </tbody>
+            </table>
+        </>
+    )
+}
+function categoryoperation(data,transcript,index,lang){
+    let detail,value,type
+    
+    switch (data.type) {
+        case 'create_account':
+            type = transcript.type.create_account
+            value={
+                a:data.funder.substring(0,4),
+                b:data.account.substring(0,4),
+                amount:parseFloat(data.starting_balance)
+            }
+            detail = formatTrans(transcript.create_account,value)
+            break;
+        case 'payment':
+            type = transcript.type.payment
+            value={
+                a:data.from.substring(0,4),
+                b:data.to.substring(0,4),
+                amount:parseFloat(data.amount)
+            }
+            detail = formatTrans(transcript.payment,value)
+            break;
+        case 'create_claimable_balance':
+            type = transcript.type.create_claimable_balance
+            value={
+                a:data.sponsor.substring(0,4),
+                b:data.claimants[1].destination.substring(0,4),
+                amount:parseFloat(data.amount)
+            }
+            detail = formatTrans(transcript.create_claimable_balance,value)
+            break;
+        case 'claim_claimable_balance':
+            type = transcript.type.claim_claimable_balance
+            value={
+                a:data.source_account.substring(0,4)
+            }
+            detail = formatTrans(transcript.claim_claimable_balance,value)
+            break;
+        default:
+            break;
+    }
+    return (
+        <tr key={index} className="bg-white border-b">
+        <td className="py-2 text-md font-medium text-gray-900">
+            <Link href={`/${lang}/pibrowser/explorer/account/${data.source_account}`}>
+                <span className=" inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-purple-400 text-white rounded-full">
+                {data.source_account.substring(0,4)}
+                </span>
+            </Link>
+        </td>
+        <td className="text-md text-gray-900 font-light py-2 break-words">
+            {type}
+        </td>
+        <td className="text-md text-gray-900 font-light py-2 break-words">
+            {detail}
+        </td>
+        </tr> 
+    )
+}
---
+++ website/app/[lang]/pibrowser/explorer/tx/[tx_hash]/page.jsx
@@ -0,0 +1,33 @@
+
+import Operation from "./op";
+import Dashboard from "./dashboard";
+import Share from "./share";
+
+export default async function TxPage({params:{lang,tx_hash}}){
+    
+    const transcript = await import(`locales/${lang}.json`);
+    return(
+        <>
+        <section className="my-2">
+            <div className="text-center text-2xl font-mono font-semibold border-b mx-4">
+                Transaction 
+                <div className=" break-words">
+                    {tx_hash}
+                </div>
+                <Share tx_hash={tx_hash}/>
+            </div>
+            
+            <div className="text-center pb-2 text-purple-500 text-lg my-2 border-b mx-4" >
+                <Dashboard tx_hash={tx_hash}/>                 
+            </div>
+        </section>
+
+        <section className="mx-2">
+            <div className="text-center mb-2 font-bold text-lg bg-border bg-border-size bg-no-repeat bg-left-bottom">
+                Operation
+            </div>
+           <Operation tx_hash={tx_hash} transcript={transcript.explorer.operation}/>
+        </section>
+        </>
+    )
+}
