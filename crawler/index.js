75ddaaa39c3a442157b35131813582b89b3a5555
lazytime <74979251+0205miss@users.noreply.github.com>
2023-10-27 03:12:33 +0800

    delete file console

--- crawler/index.js
+++ crawler/index.js
@@ -1,12 +1,3 @@
-var fs = require('fs');
-var util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/console-log.txt', {flags : 'w'});
-var log_stdout = process.stdout;
-
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
-};
 require('./lib/block')
 require('./lib/database')
 require('./lib/effect')
