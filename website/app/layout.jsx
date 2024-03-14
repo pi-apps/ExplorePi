25dc81a8f8b8f17f9b0d39bdddbf39eae1461767
lazytime <0205miss@gmail.com>
2024-03-14 17:37:09 +0800

    Sidebar flowbite remove

--- website/app/layout.jsx
+++ website/app/layout.jsx
@@ -3,13 +3,18 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Head from './head'
 import GetUser from './getuser'
-
+import { Inter } from 'next/font/google'
+ 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 config.autoAddCss = false
 
 export default function RootLayout({ children,params }) {
   
   return (
-    <html lang={params.lang}>
+    <html lang={params.lang} className={inter.className}>
       <Head />
       <body>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3524983749365405"
